cc396af939135c291ba548875b03633a7954bf4d
Jacek Nowak <jacek.jan.nowak@gmail.com>
2022-02-28 17:09:03 +0000

    work list and about page added

--- The Complete 2021 Web Development Bootcamp by Angela Yu/todolist-v1/app.js
+++ The Complete 2021 Web Development Bootcamp by Angela Yu/todolist-v1/app.js
@@ -4,37 +4,52 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-var items = [];
-
+let items = [];
+let workItems = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req,res) {
-  
-  var today = new Date();
-  
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-
-  var day = today.toLocaleDateString('en-US', options);
+let day = today.toLocaleDateString('en-US', options);
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
+  
+  let item = req.body.newItem;
 
-  items.push(item);
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  res.redirect("/");
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work List", newListItems: workItems});
+});
+app.get("/about", function(req,res) {
+  res.render('about');
+});
+
+app.post('/work', function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
 });
 
 app.listen(3000, function() {
